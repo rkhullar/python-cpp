5377d13dc1def0e40558067626693ee77984d01b
snort <talbisha@nyit.edu>
2016-04-25 18:55:48 -0400

    general update

--- builder/meta-test.py
+++ builder/meta-test.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python
-
 from meta import *
+import sys
+#print to screen on the same line
+def printf(st):
+    sys.stdout.softspace=0
+    print (st),
 
+#create a new file
+def create():
+    print("creating new  file")
+    name=raw_input ("enter the name of file:")
+    extension=raw_input ("enter extension of file:")
+    try:
+        name=name+"."+extension
+        file=open(name,'w')
+        file.write("#include <iostream> \n#include <list>\n#include <string>\n")
+        file.write("using namespace std;\nint main()\n{\n")
+
+        file.close()
+        return name
+    except:
+            print("error occured")
+            sys.exit(0)
 """
-input: program
+input: program, folder
 output: None
-creates cpp project
+creates folder with cpp project
 """
 def build(program):
 	# massive code
-	pass
+	o = program.main[0]
+	print("#include <iostream> \n#include <list>\n#include <string>")
+	print("using namespace std;\nint main()\n{")
+	if str(o.__class__) == 'meta.variable':
+		print "    "+ str(o.type), o.name,"=", str(o.value) + ";"
+
 
 if __name__ == '__main__':
+	"""	file=open(create(),'a')"""
 	p = program()
 	p.addvar('a', 'int', 4)
 	p.addstmt('assign', 'a', 5)
 	p.addstmt('print', 'a')
-	print p
+	"""print p"""
 	build(p)
+	"""file.close()"""
