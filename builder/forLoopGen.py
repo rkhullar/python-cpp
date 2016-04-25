afd1366a00f6ec8b96c28aa6ef8d833b84321cd7
snort <talbisha@nyit.edu>
2016-04-25 17:49:46 -0400

    general update

--- builder/forLoopGen.py
+++ builder/forLoopGen.py
@@ -1,36 +1,5 @@
 #!/usr/bin/python
-
-import xml.sax
 import sys
-
-class xmlHandler( xml.sax.ContentHandler ):
-   def __init__(self):
-       self.cout= ""
-       self.str = ""
-   # Call when an element starts
-   def startElement(self, tag, att):
-       if tag == "print":
-           self.cout = tag
-           printf ("cout<<\"")
-           file.write("cout<<\"")
-
-       elif tag == "string":
-            self.str = tag
-
-   # Call when an elements ends
-   def endElement(self, tag):
-       if tag == "string":
-           printf ("\"")
-           file.write("\"")
-       elif tag == "print":
-           printf (";")
-           file.write(";")
-
-   # Call when a character is read
-   def characters(self, content):
-       if self.str== "string" and content != "\t" and content != "\n":
-           printf (content)
-           file.write(content)
 
 
 #print to screen on the same line
@@ -58,12 +27,5 @@ def create():
 if ( __name__ == "__main__"):
     file=open(create(),'a')
 
-    # create an XMLReader
-    parser = xml.sax.make_parser()
-    # turn off namepsaces
-    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
-    # override the default ContextHandler
-    Handler = xmlHandler()
-    parser.setContentHandler( Handler )
-    parser.parse("input.xml")
+
     file.close()
